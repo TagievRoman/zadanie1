29be3b477bbd13fb1b185ad980b1338584ab5c6b
TagievRoman <bjenk@mail.ru>
2020-06-01 13:35:42 +0400

    Первый commit

--- lecture1.php
+++ lecture1.php
@@ -6,46 +6,46 @@ $len=strlen($file_array);
 $mas=[];
 $res=[];
 $sum=[];
-$a=0;
-$b=0;
-$c=0;
-$s=1;
-for ($i=0;$i<$len;$i++)
+$m=0;
+$s=0;
+$v=0;
+$b=1;
+for ($i=0;$i<$len;$i++) #Заносит слова в массив
 {
     if ($file_array[$i]===' ')
     {
-        $mas[$b]=substr($file_array,$a,$i-$a);
-        $a=$i+1;
-        $b=$b+1;
+        $mas[$s]=substr($file_array,$m,$i-$m);
+        $m=$i+1;
+        $s=$s+1;
 
 
     }
 }
-$lenn=count($mas);
+$lenn=count($mas); #Длинна массива
 for($i=0;$i<$lenn;$i++)
 {
-    $res[$c]=$mas[$i];
+    $res[$v]=$mas[$i];
     for($j=0;$j<$lenn;$j++)
     {
 
         if ($mas[$i]===$mas[$j] and $i!=$j)
         {
-            if ($mas[$j]!="---") {
-                $s++;
+            if ($mas[$j]!="***") {
+                $b++;
             }
-            $mas[$j]="---";
+            $mas[$j]="***";
 
         }
 
     }
-    $sum[$c]=$s;
-    $s=1;
-    $c++;
+    $sum[$v]=$b;
+    $b=1;
+    $v++;
 }
 
-for($i=0;$i<84;$i++)
+for($i=0;$i<84;$i++) #Вывод
 {
-    if ($res[$i]!="---") {
+    if ($res[$i]!="***") {
         echo $res[$i];
         echo ":";
         echo $sum[$i];
